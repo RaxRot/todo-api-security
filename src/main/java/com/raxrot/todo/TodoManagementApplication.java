ea76c1ca12dc77fc9e4f3d3370211fa42b52cb33
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-03-11 20:09:50 +0000

    Add Documentation

--- src/main/java/com/raxrot/todo/TodoManagementApplication.java
+++ src/main/java/com/raxrot/todo/TodoManagementApplication.java
@@ -1,13 +1,38 @@
 package com.raxrot.todo;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Todo REST API with Security",
+                description = "Spring Boot RESTful API with authentication and authorization (Admin & User roles). Provides CRUD operations.",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Vlad",
+                        email = "dasistperfektos@gmail.com",
+                        url = "https://github.com/RaxRot"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "GitHub Repository",
+                url = "https://github.com/RaxRot"
+        )
+)
+
 public class TodoManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoManagementApplication.class, args);
     }
-
 }
--- src/main/java/com/raxrot/todo/controller/TodoController.java
+++ src/main/java/com/raxrot/todo/controller/TodoController.java
@@ -2,6 +2,10 @@ package com.raxrot.todo.controller;
 
 import com.raxrot.todo.dto.TodoDto;
 import com.raxrot.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//http://localhost:8080/swagger-ui/index.html for work
+
+@Tag(name = "Todo Management", description = "CRUD operations for managing Todo tasks")
 @RestController
 @RequestMapping("api/todos")
 public class TodoController {
@@ -19,6 +26,11 @@ public class TodoController {
         this.todoService=todoService;
     }
 
+    @Operation(summary = "Create a new Todo", description = "Accessible only for ADMIN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Todo successfully created"),
+            @ApiResponse(responseCode = "400", description = "Validation error")
+    })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto>createTodo(@Valid @RequestBody TodoDto todoDto){
@@ -26,6 +38,11 @@ public class TodoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTodo);
     }
 
+    @Operation(summary = "Get a Todo by ID", description = "Accessible for ADMIN and USER")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo found"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/{id}")
     public ResponseEntity<TodoDto>getTodoById(@PathVariable("id")Long todoId){
@@ -35,12 +52,20 @@ public class TodoController {
                 ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Get all Todos", description = "Accessible for ADMIN and USER")
+    @ApiResponse(responseCode = "200", description = "List of todos retrieved successfully")
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>>getTodos(){
         return ResponseEntity.ok(todoService.getAllTodos());
     }
 
+    @Operation(summary = "Update a Todo", description = "Accessible only for ADMIN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Todo not found"),
+            @ApiResponse(responseCode = "400", description = "Validation error")
+    })
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<TodoDto>updateTodo(@PathVariable("id")Long todoId,@Valid @RequestBody TodoDto todoDto){
@@ -48,13 +73,23 @@ public class TodoController {
         return ResponseEntity.ok(updatedTodo);
     }
 
+    @Operation(summary = "Delete a Todo", description = "Accessible only for ADMIN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Todo deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void>deleteTodo(@PathVariable("id") Long todoId){
         todoService.deleteTodo(todoId);
         return ResponseEntity.noContent().build();
     }
-    
+
+    @Operation(summary = "Mark Todo as completed", description = "Accessible for ADMIN and USER")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo marked as completed"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping({"/{id}/complete"})
     public ResponseEntity<TodoDto>completeTodo(@PathVariable("id") Long todoId){
@@ -62,6 +97,11 @@ public class TodoController {
         return ResponseEntity.ok(updatedTodo);
     }
 
+    @Operation(summary = "Mark Todo as incomplete", description = "Accessible for ADMIN and USER")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo marked as incomplete"),
+            @ApiResponse(responseCode = "404", description = "Todo not found")
+    })
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("/{id}/incomplete")
     public ResponseEntity<TodoDto>inCompleteTodo(@PathVariable("id")Long id){
